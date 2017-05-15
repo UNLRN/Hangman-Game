0fe59d8e4b46898d72bf4b4ab6363bc8dbf100b7
Matt J <unlrn@users.noreply.github.com>
2017-05-14 21:07:41 -0400

    start css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,94 +1,73 @@
-/*
-arrays
-	list of words
-	letters in word
-	letters guessed
+(function() {
+
+    const words = ['cat', 'dog', 'horse'];
+    let lettersInWord, numGuesses, inputElements;
+    let guessedLetters = [];
+    let correctGuesses = [];
+
+    let randomWord = arr => {
+        const index = Math.floor(Math.random() * arr.length);
+        return arr[index];
+    };
 
 
-variables
-	number of guesses
+    lettersInWord = randomWord(words).split("");
+    let elGameWord = document.getElementById("game-word")
+    for (let i = 0; i < lettersInWord.length; i++) {
+        let el = document.createElement("INPUT");
+        el.setAttribute("type", "text");
+        el.setAttribute("class", "form-control");
+        el.setAttribute("answer", lettersInWord[i])
+        elGameWord.appendChild(el);
+    };
 
+    inputElements = document.getElementsByTagName("INPUT");
 
+    let alreadyGuessed = (letter) => {
+        if (guessedLetters.indexOf(letter) > -1) {
+            return true;
+        } else {
+            return false;
+        };
+    };
 
+    let correctGuess = (letter) => {
+        if (lettersInWord.indexOf(letter) > -1) {
+            return true;
+        } else {
+            return false;
+        };
+    };
 
+    let getKey = e => {
+        let x = event.which;
+        x = (String.fromCharCode(x));
+        if (alreadyGuessed(x)) {
+            alert("You already Guessed this letter");
+        } else {
+            if (correctGuess(x)) {
+                inputElements[lettersInWord.indexOf(x)].value = x;
+                let el = document.getElementById(x)
+                el.setAttribute("style", "background-color: green; color: white;")
+                guessedLetters.push(x);
+                correctGuesses.push(x);
+                if (lettersInWord.length === correctGuesses.length) {
+                    alert("You Win");
+                }
+            } else {
+                guessedLetters.push(x);
+                numGuesses = document.getElementById("guesses").innerHTML;
+                if (parseInt(numGuesses) === 0) {
+                    alert("You Lose")
+                } else {
+                    numGuesses -= 1
+                    document.getElementById("guesses").innerHTML = numGuesses.toString();
+                    let el = document.getElementById(x);
+                    el.setAttribute("style", "background-color: red; color: white")
+                };
+            };
+        };
+    };
 
-*/
-
-
-
-
-
-(function () {
-
-	const words = ['cat', 'dog', 'horse'];
-	let lettersInWord, numGuesses, inputElements;
-	let guessedLetters = [];
-
-	let randomWord = arr => {
-		const index = Math.floor(Math.random() * arr.length);
-		return arr[index];
-	};
-	
-
-	lettersInWord = randomWord(words).split("");
-	// console.log(word);
-	for (let i=0; i < lettersInWord.length; i++) {
-		let el = document.createElement("INPUT");
-		el.setAttribute("type", "text");
-		el.setAttribute("answer", lettersInWord[i])
-		document.body.appendChild(el);
-	};
-
-	
-
-	inputElements = document.getElementsByTagName("INPUT");
-
-	let alreadyGuessed = (letter) => {
-		if (guessedLetters.indexOf(letter) > -1) {
-			return true;
-		} else {
-			return false;
-		};
-	};
-
-	let correctGuess = (letter) => {
-		if (lettersInWord.indexOf(letter) > -1) {
-			return true;
-		} else {
-			return false;
-		};
-	};
-
-	let getKey = e => {
-		let x = event.which;
-		x = (String.fromCharCode(x));
-		if (alreadyGuessed(x)) {
-			alert("You already Guessed this letter");
-		} else {
-			if (correctGuess(x)) {
-				inputElements[lettersInWord.indexOf(x)].value = x;
-				guessedLetters.push(x);
-			} else {
-				guessedLetters.push(x);
-				numGuesses = document.getElementById("guesses").innerHTML;
-				if (parseInt(numGuesses) < 0) {
-					alert("You Lose")
-				} else {
-					numGuesses -= 1
-					document.getElementById("guesses").innerHTML = numGuesses.toString();
-					let el = document.getElementById('letters-container');
-					let newEl = document.createElement('DIV');
-					newEl.setAttribute('class', 'guessed-letter');
-					newEl.innerHTML = x
-					el.appendChild(newEl);
-				};
-			};
-		};
-	};
-
-	window.addEventListener("keypress", getKey);
-
-
-
-
+    window.addEventListener("keypress", getKey);
 }());
